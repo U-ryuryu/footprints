ff5af9271bb1436547977075d68218b586d8708e
U-ryuryu <kugyuryuryu@gmail.com>
2021-04-22 19:36:39 +0900

    ユーザー編集機能の実装

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,9 +1,9 @@
 class ClientsController < ApplicationController
   def index
     if admin_signed_in?
-      @clients = Client.where(admin_id: current_admin.id)
+      @client = Client.where(admin_id: current_admin.id)
     elsif user_signed_in?
-      @clients = Client.where(admin_id: current_user.admin.id)
+      @client = Client.where(admin_id: current_user.admin.id)
     end
   end
 
@@ -12,7 +12,7 @@ class ClientsController < ApplicationController
   end
 
   def create
-    @client = Client.new(params_client)
+    @client = Client.new(client_params)
     if @client.save
       redirect_to root_path
     else
@@ -20,9 +20,22 @@ class ClientsController < ApplicationController
     end
   end
 
+  def edit
+    @client = Client.find(params[:id])
+  end
+
+  def update
+    @client = Client.find(params[:id])
+    if @client.update(client_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
   private
 
-  def params_client
+  def client_params
     if admin_signed_in?
       params.require(:client).permit(:name, :tel, :postal_code, :address, :charge, :charge_tel).merge(admin_id: current_admin.id)
     else
