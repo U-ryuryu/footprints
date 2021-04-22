cc36498ea27bb82419e316d9f3a7680f42fb98d5
U-ryuryu <kugyuryuryu@gmail.com>
2021-04-22 19:08:23 +0900

    顧客情報作成ページの実装

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,4 +1,32 @@
 class ClientsController < ApplicationController
   def index
+    if admin_signed_in?
+      @clients = Client.where(admin_id: current_admin.id)
+    elsif user_signed_in?
+      @clients = Client.where(admin_id: current_user.admin.id)
+    end
+  end
+
+  def new
+    @client = Client.new
+  end
+
+  def create
+    @client = Client.new(params_client)
+    if @client.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def params_client
+    if admin_signed_in?
+      params.require(:client).permit(:name, :tel, :postal_code, :address, :charge, :charge_tel).merge(admin_id: current_admin.id)
+    else
+      params.require(:client).permit(:name, :tel, :postal_code, :address, :charge, :charge_tel).merge(admin_id: current_user.admin.id)
+    end
   end
 end
