5d16c59657cbcb45e1bc6b53fb3ca6c5a96d0e06
U-ryuryu <kugyuryuryu@gmail.com>
2021-05-08 23:26:47 +0900

    Active Storageの導入,アソシエーションの設定

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -89,7 +89,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :image])
   end
 
   def sign_up(resource_name, resource)
@@ -100,7 +100,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :image])
   end
 
   def authenticate_scope!
